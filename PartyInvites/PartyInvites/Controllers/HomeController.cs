e1d1dd5baf4c9d06607dc248545470de601db2cd
Lukasz Markiewicz <markiewicz.lukasz@hotmail.com>
2016-05-17 15:18:32 +0200

    Całość projektu PartyInvites

--- PartyInvites/PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PartyInvites.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,6 +15,26 @@ namespace PartyInvites.Controllers
             int hour = DateTime.Now.Hour;
             ViewBag.Greeting = hour < 17 ? "Dzień Dobry" : "Dobry Wieczór";
             return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                //do zriobiena: wyślij zawartość gusetResponse do Organizatora przyjęcia
+                return View("Thanks", guestResponse);
+            }
+            else
+            { //błąd kontroli poprawności, więc ponownie wyświetlamy formularz wprowadzania danych
+                return View();
+            }
+            
         }
     }
 }
--- PartyInvites/PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/PartyInvites/Models/GuestResponse.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,18 @@ namespace PartyInvites.Models
 {
     public class GuestResponse
     {
+        [Required(ErrorMessage = "Proszę podać swoje imię i Nazwisko.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać adres e-mail.")]
+        [RegularExpression(".+\\@.+\\..+",
+            ErrorMessage = "Proszę podać prawidłowy adres e-mail.")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać prawidłowy numer telefonu.")]
         public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Proszę określić, czy weźmiesz udział.")]
         public bool? WillAttend { get; set; }
     }
 }
