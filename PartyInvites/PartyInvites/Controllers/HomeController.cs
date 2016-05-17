c4cdfc4851b8496a1d702159337c9d62d2748ad6
Lukasz Markiewicz <markiewicz.lukasz@hotmail.com>
2016-05-17 13:02:51 +0200

    Rozpoczęcie projektu dodanie vidoku

---
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 17 ? "Dzień Dobry" : "Dobry Wieczór";
+            return View();
+        }
+    }
+}
